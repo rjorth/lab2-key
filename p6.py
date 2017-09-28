bb355aa714844486d0e517b33989d71c8f1e34ae
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-09-28 04:17:10 -0400

    Create p6.py
    files p1-p5 only need to be tested. File p6.py is still worthless.

---
+++ p6.py
@@ -0,0 +1,29 @@
+class TextProcessor:
+    def __init__(self, test_string):
+        self.test_string = ''
+
+        def count_alphabet():
+            count = 0
+                for char in test_string:
+                    count += 1
+                    return count
+
+        def vowel_capitalization():
+            to_upper = ['a', 'e', 'i', 'o', 'u']
+            array = []
+            for c in test_string:
+                if c in to_upper:
+                    c = c.upper()
+                array.append(c)
+            print(''.join(array))
+
+            def concat(new_string):
+                ''.join(test_string, new_string)
+
+            def search(sub):
+                sub = ''
+                try: 
+                    test_string.find(sub)
+                except ValueError: 
+                    return -1
+               
